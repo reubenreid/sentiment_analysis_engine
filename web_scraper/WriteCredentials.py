f97a6c020d2d8b04c58c2c210592e329603b867e
Reuben Reid <mz020778@live.reading.ac.uk>
2020-02-19 10:51:02 +0000

    whoops

--- web_scraper/WriteCredentials.py
+++ web_scraper/WriteCredentials.py
@@ -2,10 +2,10 @@ import json
 
 twitter_cred = dict()
 
-twitter_cred['CONSUMER_KEY'] = '37gDZNQZbwfwr99AqMkDZ35jt'
-twitter_cred['CONSUMER_SECRET'] = 'p8aJ72M7ZlRfAvv50hj2IL8D9mWBO79KhxI28euaHS7tOfc7Uv'
-twitter_cred['ACCESS_KEY'] = '1193840436508602368-8Q0WO9pOvjrQbys7A0XbvrszTNPB2V'
-twitter_cred['ACCESS_SECRET'] = 'o7KokHPdUIkNibMxpkEo86eeSEn0IeZQFn271LGiShQQO'
+twitter_cred['CONSUMER_KEY'] = ''
+twitter_cred['CONSUMER_SECRET'] = ''
+twitter_cred['ACCESS_KEY'] = ''
+twitter_cred['ACCESS_SECRET'] = ''
 
 with open('twitter_credentials.json', 'w') as secret_info:
     json.dump(twitter_cred, secret_info, indent=4, sort_keys=True)
